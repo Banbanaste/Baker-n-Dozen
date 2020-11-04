4fffbf2b8f1045a6722dac9332d287bf9e0c1a85
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-11-03 20:44:10 -0500

    web v2.01

--- components/bottomNav.js
+++ components/bottomNav.js
@@ -12,9 +12,9 @@ import ContactButton from "./contactButton";
 export default function Nav() {
   return (
     <nav className="bg-black text-white fixed bottom-0 larger:hidden w-full z-10">
-      <div className="flex flex-col justify-center items-center h-full font-bold uppercase container mx-auto justify-between py-6 px-4">
-        <ContactButton classes="hover:underline font-bold leading-none tracking-wider cursor-pointer mb-6" />
-        <div className="flex text-3xl w-1/2 justify-between items-center">
+      <div className="flex flex-col justify-between items-center h-full font-bold uppercase container mx-auto justify-between py-6 px-4">
+        <div className="flex text-3xl justify-between w-full items-center">
+          <ContactButton classes="hover:underline text-base font-bold leading-none tracking-wider cursor-pointer" />
           <a href="" className="hover:text-very_blue cursor-pointer text-2xl">
             <MdMailOutline />
           </a>
