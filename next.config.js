47c12c023de440584e371b986bb3fda0fe9559df
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-11-19 02:57:56 -0500

    web v8.01 | env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  env: {
-    CONTENTFUL_SPACE_ID: "03it9wps3kh4",
-    CONTENTFUL_ACCESS_TOKEN: "wbrxSXhe5SHhdNIJDhDBMN0eaOBxtQKu4FPg0gyeQIk",
-  },
-};
+// module.exports = {
+//   env: {
+//     CONTENTFUL_SPACE_ID: "03it9wps3kh4",
+//     CONTENTFUL_ACCESS_TOKEN: "wbrxSXhe5SHhdNIJDhDBMN0eaOBxtQKu4FPg0gyeQIk",
+//   },
+// };
