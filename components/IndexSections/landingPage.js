42a30b6dd3d0432e90db1a13f94f77a36668f608
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-11-20 17:50:35 -0500

    web v9.06 | img op

--- components/IndexSections/landingPage.js
+++ components/IndexSections/landingPage.js
@@ -10,12 +10,10 @@ export default function LandingPage() {
     <section
       aria-label="Schedule an Appointment to help your business."
       className="section-default overflow relative"
+      data-aos="fade"
       style={{}}
     >
-      <div
-        className="absolute top-0 right-0 pointer-events-none w-7/12 flex justify-end"
-        data-aos="fade"
-      >
+      <div className="absolute top-0 right-0 pointer-events-none w-7/12 flex justify-end">
         <Image
           src="/images/bison.png"
           alt="Ooh a Bison"
@@ -28,14 +26,7 @@ export default function LandingPage() {
 
       <div className="container text-light flex justify-start xl:pl-32 z-20">
         <div className="max-w-84 2xl:max-w-96">
-          <div
-            className="mb-4"
-            data-aos="fade-down"
-            data-aos-delay="100"
-            data-aos-duration="500"
-            data-aos-easing="ease"
-            data-aos-once="true"
-          >
+          <div className="mb-4">
             <Image
               src="/images/logo_icon_red.png"
               alt="Caveman Consulting Co. Logo"
@@ -44,47 +35,20 @@ export default function LandingPage() {
               priority
             />
           </div>
-          <h1
-            className="text-5xl phone:text-3xl font-bold mb-6 z-10"
-            data-aos="fade-down"
-            data-aos-delay="200"
-            data-aos-duration="500"
-            data-aos-easing="ease"
-            data-aos-once="true"
-          >
+          <h1 className="text-5xl phone:text-3xl font-bold mb-6 z-10">
             Bring your business out of the Stone Age
           </h1>
-          <p
-            className="text-3xl phone:text-base font-medium mb-12 z-10"
-            data-aos="fade-down"
-            data-aos-delay="450"
-            data-aos-duration="500"
-            data-aos-easing="ease"
-            data-aos-once="true"
-          >
+          <p className="text-3xl phone:text-base font-medium mb-12 z-10">
             with a modern web experience and marketing campaign anything is
             possible
           </p>
           <div className="w-full flex flex-wrap">
-            <span
-              className=" mr-10 mb-6"
-              data-aos="fade-down"
-              data-aos-delay="550"
-              data-aos-duration="500"
-              data-aos-easing="ease"
-              data-aos-once="true"
-            >
+            <span className=" mr-10 mb-6">
               <ContactBtn classes="duration-300 ease rounded-lg px-8 phone:px-4 py-2 bg-light text-burg cursor-pointer font-bold hover:opacity-50 ">
                 Schedule a Consult
               </ContactBtn>
             </span>
-            <div
-              data-aos="fade-down"
-              data-aos-delay="650"
-              data-aos-duration="500"
-              data-aos-easing="ease"
-              data-aos-once="true"
-            >
+            <div>
               <span className="duration-300 ease hover:opacity-50 font-bold ">
                 <Link href="/#about">More Info</Link>
               </span>
