a3ee940afdcee107b6c43a8665d11de3e2052604
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-11-19 20:39:03 -0500

    web v9.03

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -7,22 +7,22 @@ import ContactBtn from "../templates/contactButton";
 
 export default function Nav(props) {
   return (
-    <nav className="transform duration-500 w-full phone:hidden fixed top-0 z-50 pt-10 px-4">
-      <div className="container mx-auto flex justify-center items-center uppercase text-white py-2 space-x-10 tracking-widest text-sm font-bold">
+    <nav className="transform duration-500 w-full phone:hidden fixed top-0 z-50 py-2 px-4 bg-dark">
+      <div className="container mx-auto flex justify-center items-center uppercase text-white space-x-10 tracking-widest text-sm font-bold">
         <Link href="/">
           <a className="text-sm">
             <Image
               src="/images/logo_icon.png"
               alt="Caveman Consulting Co. Logo"
-              height="55"
-              width="55"
+              height="45"
+              width="45"
             />
           </a>
         </Link>
         <Link href="/marketing">Marketing</Link>
         <Link href="/web">Web</Link>
         <Link href="/#projects">Projects</Link>
-        <ContactBtn classes="cursor-pointer bg-dark rounded-lg px-4 py-1">
+        <ContactBtn classes="cursor-pointer bg-light text-light_burg rounded-lg px-4 py-1 duration-300 hover:opacity-50">
           Contact
         </ContactBtn>
       </div>
