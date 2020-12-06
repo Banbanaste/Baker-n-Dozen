e3301397513e314f7d46d2aa2ae845f19ba0c072
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-12-06 11:44:35 -0500

    sale update

--- pages/sale.js
+++ pages/sale.js
@@ -7,19 +7,19 @@ import Image from "next/image";
 // calendy
 import ContactButton from "../components/templates/contactButton";
 
-export default function MarketingPage() {
+export default function SalePage() {
   return (
     <Layout
       isLanding={true}
-      title="Caveman | Black Friday"
-      description="Black Friday Sale! Get a Free Landing Page with any Marketing Campaign. Save $500 Today! Book a 15 minute consultation! Sale Ends 12/4/2020."
+      title="Caveman | End of Year"
+      description="End of Year Sale! Get a Free Landing Page with any Marketing Campaign. Save $500 Today! Book a consultation! Sale Ends 12/31/2020."
     >
       <div className="bg-tone min-h-screen">
         <section className="py-10 bg-white">
           <div data-aos="fade" className="container text-center mb-10 mx-auto">
-            <h2 className="text-xl text-gray-700 mb-2">ENDS 12/04</h2>
+            <h2 className="text-xl text-gray-700 mb-2">ENDS 12/31</h2>
             <h1 className="text-5xl font-bold text-dark mb-4">
-              BLACK FRIDAY SALE
+              END OF YEAR SALE
             </h1>
             <h3 className="text-2xl font-bold text-dark mb-12">
               Free Tailored Landing Page with any Marketing Campaign. $500
